85d761df5709e5ba585dd0591dccd0a27c5c5780
Dream <166921709+hamoudi223@users.noreply.github.com>
2025-06-29 03:27:34 +0000

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,16 @@
 export const config = {
   pastebinApiKey: 'Tto1zQt8Ma5j7h_K9E_FG7fZJWhFDcIx',
-  welcomeMessage: 'Salut, ta session est prête ! Merci!! Voici ton lien pour ta session complète:',
+  welcomeMessage: 'Bonjour ! 🐳
+
+Ta session WhatsApp a bien été générée et sauvegardée en toute sécurité.
+
+Voici ton code de session unique : 
+${sessionShortCode}
+
+Conserve bien ce code, il te permettra de récupérer ta session complète.
+
+Merci d'utiliser notre service !
+  ',
   welcomeImageUrl: 'https://i.imgur.com/4AiXzf8.jpeg',
   port: 3000,
 };
