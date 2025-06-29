0a8d1a2aeddc6774ee5cc8aca0b6da455516aa1c
Dream <166921709+hamoudi223@users.noreply.github.com>
2025-06-29 03:21:34 +0000

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  pastebinApiKey: 'Tto1zQt8Ma5j7h_K9E_FG7fZJWhFDcIx',
+  welcomeMessage: 'Salut, ta session est prête ! Merci!! Voici ton lien pour ta session complète:',
+  welcomeImageUrl: 'https://i.imgur.com/4AiXzf8.jpeg',
+  port: 3000,
+};
